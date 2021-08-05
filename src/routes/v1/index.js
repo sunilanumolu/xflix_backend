af7f066dfccf8f2afdec68de2a4fb2908fa60bce
Venkata Sunil Kumar Anumolu <32481223+sunilanumolu@users.noreply.github.com>
2021-08-05 11:27:10 +0530

    Update index.js

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const videoRoute = require("./video.route");
 // const captureDateMiddleware = require("../../middleware/middleware");
 const router = express.Router();
-
+router.use("*", videoRoute);//heroku working ke liye
 router.use("/videos", videoRoute);
 
 module.exports = router;
