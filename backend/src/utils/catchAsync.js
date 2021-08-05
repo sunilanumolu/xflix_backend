32ca4d8e0240c8667040b6f89af999ef44932765
Venkata Sunil Kumar Anumolu <32481223+sunilanumolu@users.noreply.github.com>
2021-08-05 10:54:59 +0530

    Delete backend directory

--- backend/src/utils/catchAsync.js
+++
@@ -1,7 +0,0 @@
-function catchAsync(functn) {
-    return function(req, res, next) {
-      Promise.resolve(functn(req, res, next)).catch((err) => next(err));
-    }
-  }
-  
-  module.exports = catchAsync;
